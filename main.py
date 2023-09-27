7dcc444e5d35a387512f665263788cca7a6f62d1
coumbaseck <coumba.seck@toulouse.miage.fr>
2023-09-27 09:32:31 +0200

    Modif fetch_genre_song function

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ def main():
     # Appel de la fonction pour récupérer les données de l'API
     popularity_data = functions.fetch_genre_popularity(url)
 
-    #genre_data = functions.fetch_genre_song(url)
+    genre_data = functions.fetch_genre_song(url)
 
     if popularity_data:
         # Utilisez les données récupérées comme bon vous semble
         print(popularity_data)
 
-    #if genre_data :
+    if genre_data :
 
-        #print(genre_data)
+        print(genre_data)
 
 
 if __name__ == "__main__":
