63187d11a21b72128022cf7280e073b46284a3b3
DESKTOP-C96R9CL\kharb <kharbach2320@gmail.com>
2023-10-25 23:53:09 +0200

    details v2 ( orga de chaque sous genre dans un rep genre ) et recup des details des artistes sans genres

--- main.py
+++ main.py
@@ -10,15 +10,20 @@ def main():
     """Appel de la fonction pour récupérer les données de l'API"""
     #popularity_data = functions.fetch_genre_popularity(url)
 
-    artist_data = functions.fetch_artists_all(url)
-
-    if artist_data:
-
-        print(artist_data)
+    # artist_data = functions.fetch_artists_all(url)
+    #
+    # if artist_data:
+    #
+    #     print(artist_data)
 
     # functions.fetch_artists_by_genre()
 
     #functions.fetch_info_artist(url)
+
+    # functions.fetch_details()
+    functions.fetch_details_others()
+
+
 
 
 
