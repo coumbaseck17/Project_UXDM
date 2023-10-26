24f99033b2ab363733126dbeafa1061654852f6f
Maneva <manevarsld@gmail.com>
2023-10-27 01:55:32 +0200

    update : ajout de la méthode pour récupérer artistes triés par genre, méthode pour récupérer les détails d'un GENRE global, méthode pour renvoyer artistes en fonction filtre

--- functions.py
+++ functions.py
@@ -1,15 +1,24 @@
 import os
 import urllib
 import time
+
+import genre as genre
 import requests
 import json
+
+from main import Subgenre
+from main import Genre
+from main import FilterValue
+from main import FilterType
 
 url = "https://wasabi.i3s.unice.fr"
 
 params = {}
 
 """Effectue une requête GET à l'API avec les paramètres spécifiés."""
-def api_request(url, endpoint, params) :
+
+
+def api_request(url, endpoint, params):
     try:
         response = requests.get(url + endpoint, params)
 
@@ -25,10 +34,10 @@ def api_request(url, endpoint, params) :
 
 
 """recuperer les genres plus populaires"""
+
+
 def fetch_genre_popularity(url):
-
     endpoint = "/api/v1/artist/genres/popularity"
-
 
     popularity_data = api_request(url, endpoint, params)
 
@@ -44,9 +53,9 @@ def fetch_genre_popularity(url):
     return popularity_data
 
 
-
-
 """Fonction pour récupérer les noms des artistes en fonction d'un genre depuis l'API Wasabi"""
+
+
 def fetch_artists_by_genre(url):
     endpoint = "/search/genre/"
 
@@ -85,7 +94,6 @@ def fetch_artists_by_genre(url):
     return genre_data
 
 
-
 # def fetch_info_artist(url):
 #
 #     endpoint = "/search/artist/"
@@ -120,17 +128,19 @@ def fetch_artists_by_genre(url):
 #
 #     return artist_name_list
 """ récupérer les données (sélectionnées) de tout les artistes """
+
+
 def fetch_artists_all(url):
     number = 1
     names_genres = []
-    number_str= str(number)
-    while(number < 77492):
+    number_str = str(number)
+    while (number < 77492):
         endpoint = "/api/v1/artist_all/" + number_str
         request = api_request(url, endpoint, {})
         print(endpoint)
         for entry in request:
             artist_deezerFans = entry.get("deezerFans")
-            if(artist_deezerFans is not None and artist_deezerFans != 0):
+            if (artist_deezerFans is not None and artist_deezerFans != 0):
 
                 albums = entry.get("albums")
                 members = entry.get("members")
@@ -150,20 +160,20 @@ def fetch_artists_all(url):
                     list.append(nom)
 
                 data = {
-                "id": entry.get("_id"),
-                "name": entry.get("name"),
-                "type": entry.get("type"),
-                "genres": entry.get("genres"),
-                "nombre_albums": nombre_albums,
-                "nombre_members" : nombre_members,
-                "recordLabel": entry.get("recordLabel"),
-                "urlDeezer": entry.get("urlDeezer"),
-                "gender": entry.get("gender"),
-                "picture": entry["picture"]["standard"],
-                "albums": list,
-                "lifeSpan" : entry.get("lifeSpan"),
-                "deezerFans": artist_deezerFans,
-                "members" : list_members
+                    "id": entry.get("_id"),
+                    "name": entry.get("name"),
+                    "type": entry.get("type"),
+                    "genres": entry.get("genres"),
+                    "nombre_albums": nombre_albums,
+                    "nombre_members": nombre_members,
+                    "recordLabel": entry.get("recordLabel"),
+                    "urlDeezer": entry.get("urlDeezer"),
+                    "gender": entry.get("gender"),
+                    "picture": entry["picture"]["standard"],
+                    "albums": list,
+                    "lifeSpan": entry.get("lifeSpan"),
+                    "deezerFans": artist_deezerFans,
+                    "members": list_members
                 }
                 print(data)
                 names_genres.append(data)
@@ -171,8 +181,6 @@ def fetch_artists_all(url):
         print("pause")
         number += 200
         number_str = str(number)
-
-
 
     # Enregistrer les données dans un fichier JSON (écrasée ou crée)
     file_path = "data/artist_all.json"
@@ -183,6 +191,8 @@ def fetch_artists_all(url):
 
 
 """ recuperer les details de chaque genre et sous genres """
+
+
 def fetch_details():
     with open('data/artist_all.json', 'r', encoding='utf-8') as json_file:
         dataArtist = json.load(json_file)
@@ -192,7 +202,7 @@ def fetch_details():
 
     for genre, subgenres in data.items():
         # créer un répertoire pour le genre
-        genre_dir=os.path.join('data/details', genre)
+        genre_dir = os.path.join('data/details', genre)
         os.makedirs(genre_dir, exist_ok=True)
 
         for subgenre in subgenres:
@@ -212,24 +222,20 @@ def fetch_details():
 
             total_artists = nbr_groupes + nbr_solos
             if total_artists > 0:
-                pourcentage_actif = int((nbr_actif / total_artists)*100)
+                pourcentage_actif = int((nbr_actif / total_artists) * 100)
             else:
                 pourcentage_actif = 0
 
             subgenre_info = {
                 "nombre_groupes": nbr_groupes,
                 "nombre_solos": nbr_solos,
-                "nombre_actif" : nbr_actif,
+                "nombre_actif": nbr_actif,
                 "pourcentage_actifs": pourcentage_actif
             }
 
-            subgenre_file = os.path.join(genre_dir,f'{subgenre}.json')
+            subgenre_file = os.path.join(genre_dir, f'{subgenre}.json')
             with open(subgenre_file, 'w', encoding="utf-8") as output_file:
-                json.dump(subgenre_info,output_file, indent=4)
-
-
-
-
+                json.dump(subgenre_info, output_file, indent=4)
 
             # print(f"Pour le genre {genre} et le sous-genre {subgenre}:")
             # print(f"Nombre de groupes : {nbr_groupes}")
@@ -264,10 +270,10 @@ def fetch_details_others():
         pourcentage_actif = 0
 
     others_info = {
-    "nombre_groupes": nbr_groupes,
-    "nombre_solos": nbr_solos,
-    "nombre_actif" : nbr_actif,
-    "pourcentage_actifs": pourcentage_actif
+        "nombre_groupes": nbr_groupes,
+        "nombre_solos": nbr_solos,
+        "nombre_actif": nbr_actif,
+        "pourcentage_actifs": pourcentage_actif
     }
 
     genre_dir = 'data/details'
@@ -276,24 +282,166 @@ def fetch_details_others():
         json.dump(others_info, output_file, indent=4)
 
 
+# récupérer les artistes par genre & sous-genre => RENVOYER DES DONNEES FAUSSES à mettre en commentaire plus nécessaire
+# pour l'instant
+def fetch_artists_genre():
+    with open('data/genre_subgenre.json', 'r', encoding='utf-8') as json_file:
+        genre_data = json.load(json_file)
+
+    with open('data/artist_all.json', 'r', encoding='utf-8') as json_file:
+        artist_data = json.load(json_file)
+
+    artists_by_genre = {genre: {subgenre: [] for subgenre in subgenres} for genre, subgenres in genre_data.items()}
+
+    for artist in artist_data:
+        artist_genres = artist.get("genres", [])
+        artist_type = artist.get("type", "Unknown")
+
+        for genre, subgenres in genre_data.items():
+            for subgenre in subgenres:
+                if genre in artist_genres or subgenre in artist_genres:
+                    artists_by_genre[genre][subgenre].append(artist)
+
+    output_dir = 'data/artists_by_genre'
+    os.makedirs(output_dir, exist_ok=True)
+
+    for genre, subgenres_data in artists_by_genre.items():
+        genre_dir = os.path.join(output_dir, genre)
+        os.makedirs(genre_dir, exist_ok=True)
+
+        for subgenre, artists in subgenres_data.items():
+            filename = os.path.join(genre_dir, f'{subgenre}.json')
+            with open(filename, 'w', encoding='utf-8') as json_file:
+                json.dump(artists, json_file, ensure_ascii=False, indent=4)
 
 
+# récupérer les artistes par genre & sous-genre et les trier par popularité
+def organize_and_sort_artists():
+    with open('data/genre_subgenre.json', 'r', encoding='utf-8') as json_file:
+        genre_data = json.load(json_file)
+
+    with open('data/artist_all.json', 'r', encoding='utf-8') as json_file:
+        artist_data = json.load(json_file)
+
+    artists_by_genre = {genre: {subgenre: [] for subgenre in subgenres} for genre, subgenres in genre_data.items()}
+
+    for artist in artist_data:
+        artist_genres = artist.get("genres", [])
+        artist_type = artist.get("type", "Unknown")  # Si le type est manquant, utilisez "Unknown"
+
+        for genre, subgenres in genre_data.items():
+            for subgenre in subgenres:
+                if subgenre in artist_genres:
+                    artists_by_genre[genre][subgenre].append(artist)
+
+    def classify_artists_by_deezer_fans(artists_by_genre):
+        sorted_artists = {}
+
+        for genre, subgenres_data in artists_by_genre.items():
+            sorted_artists[genre] = {}
+            for subgenre, artists in subgenres_data.items():
+                sorted_artists[genre][subgenre] = sorted(artists, key=lambda x: x.get("deezerFans", 0), reverse=True)
+
+        return sorted_artists
+
+    sorted_artists_by_deezer_fans = classify_artists_by_deezer_fans(artists_by_genre)
+
+    output_dir = 'data/artists_by_genre_sorted'
+    os.makedirs(output_dir, exist_ok=True)
+
+    for genre, subgenres_data in sorted_artists_by_deezer_fans.items():
+        genre_dir = os.path.join(output_dir, genre)
+        os.makedirs(genre_dir, exist_ok=True)
+
+        for subgenre, artists in subgenres_data.items():
+            filename = os.path.join(genre_dir, f'{subgenre}.json')
+            with open(filename, 'w', encoding='utf-8') as json_file:
+                json.dump(artists, json_file, ensure_ascii=False, indent=4)
 
 
+# récupérer les détails d'un GENRE en général
+def fetch_genre_details_ALL():
+    with open('data/genre_subgenre.json', 'r', encoding='utf-8') as json_file:
+        genre_data = json.load(json_file)
+
+    for genre, subgenres in genre_data.items():
+        genre_dir = os.path.join('data/artists_by_genre_sorted', genre)
+        genre_all_file = os.path.join(genre_dir, f'{genre}_all_artists_sorted.json')
+
+        with open(genre_all_file, 'r', encoding='utf-8') as json_file:
+            artists = json.load(json_file)
+
+        unique_artists = {}
+
+        for artist in artists:
+            artist_id = artist["id"]
+            if artist_id not in unique_artists:
+                unique_artists[artist_id] = artist
+
+        nbr_groupes = 0
+        nbr_solos = 0
+        nbr_actif = 0
+
+        for artist in unique_artists.values():
+            if artist["type"] == "Group":
+                nbr_groupes += 1
+            elif artist["type"] == "Person":
+                nbr_solos += 1
+
+            if not artist["lifeSpan"]["ended"]:
+                nbr_actif += 1
+
+        total_artists = len(unique_artists)
+        if total_artists > 0:
+            pourcentage_actif = int((nbr_actif / total_artists) * 100)
+        else:
+            pourcentage_actif = 0
+
+        genre_info = {
+            "nombre_groupes": nbr_groupes,
+            "nombre_solos": nbr_solos,
+            "nombre_actif": nbr_actif,
+            "pourcentage_actifs": pourcentage_actif
+        }
+
+        details_dir = os.path.join('data/details', genre)
+        os.makedirs(details_dir, exist_ok=True)
+        genre_info_file = os.path.join(details_dir, f'{genre}_details.json')
+        with open(genre_info_file, 'w', encoding='utf-8') as json_file:
+            json.dump(genre_info, json_file, ensure_ascii=False, indent=4)
 
 
+class FilterType:
+    TYPE = "type"
+    GENDER = "gender"
+    NONE = "none"
 
 
+# retourner les artiste par GENRE, SOUS GENRE (ALL ou SOUS GENRE) , FILTRE, VALEUR, LIMIT
+def filter_artists(genre, subgenre, filter_type=FilterType.NONE, filter_value=None, limit=None):
+    # Construire le chemin du fichier en fonction des paramètres
+    if subgenre.lower() == "all":
+        file_path = f"data/artists_by_genre_sorted/{genre}/{genre}_all_artists_sorted.json"
+    else:
+        file_path = f"data/artists_by_genre_sorted/{genre}/{subgenre}.json"
 
+    try:
+        with open(file_path, 'r', encoding='utf-8') as json_file:
+            artists = json.load(json_file)
 
+        if filter_type != FilterType.NONE and filter_value is not None:
+            if filter_type == FilterType.TYPE:
+                filtered_artists = [artist for artist in artists if artist["type"] == filter_value]
+            elif filter_type == FilterType.GENDER:
+                filtered_artists = [artist for artist in artists if artist["gender"] == filter_value]
+            else:
+                filtered_artists = artists
+        else:
+            filtered_artists = artists
 
+        if limit is not None:
+            filtered_artists = filtered_artists[:limit]
+        return filtered_artists
 
-
-
-
-
-
-
-
-
-
+    except FileNotFoundError:
+        return []
--- main.py
+++ main.py
@@ -1,8 +1,87 @@
-import time
 
 import functions
 url = "https://wasabi.i3s.unice.fr"
-import json
+
+#enums pas encore utilisé
+from enum import Enum
+
+class Genre(Enum):
+    ROCK = "Rock"
+    POP = "Pop"
+    FOLK = "Folk"
+    HIP_HOP = "Hip Hop"
+    ELECTRONIC = "Electronic"
+    BLUES = "Blues"
+    RNB = "R&B"
+    JAZZ = "Jazz"
+    COUNTRY = "Country"
+
+class Subgenre(Enum):
+    # Rock
+    ALL = "all"
+    ROCK = "Rock"
+    ALTERNATIVE_ROCK = "Alternative Rock"
+    INDIE_ROCK = "Indie Rock"
+    HEAVY_METAL = "Heavy Metal"
+    HARD_ROCK = "Hard Rock"
+    PUNK_ROCK = "Punk Rock"
+    PROGRESSIVE_ROCK = "Progressive Rock"
+    FOLK_ROCK = "Folk Rock"
+    ROCK_N_ROLL = "Rock 'N' Roll"
+    CLASSIC_ROCK = "Classic Rock"
+    HARD_METAL = "Heavy Metal"
+
+    # Pop
+    POP = "Pop"
+    SINGER_SONGWRITER = "Singer-Songwriter"
+    POP_ROCK = "Pop Rock"
+    INDIE_POP = "Indie Pop"
+    DANCE_POP = "Dance-Pop"
+    ELECTROPOP = "Electropop"
+
+    # Folk
+    FOLK = "Folk"
+    CONTEMPORARY_FOLK = "Contemporary Folk"
+    INDIE_FOLK = "Indie Folk"
+    PROGRESSIVE_FOLK = "Progressive Folk"
+    PSYCH_FOLK = "Psych Folk"
+
+    # Hip Hop
+    HIP_HOP = "Hip Hop"
+
+    # Electronic
+    ELECTRONIC = "Electronic"
+    HOUSE = "House"
+    TECHNO = "Techno"
+
+    # Blues
+    BLUES = "Blues"
+    BLUES_ROCK = "Blues Rock"
+
+    # R&B
+    SOUL = "Soul"
+    RNB = "R&B"
+    FUNK = "Funk"
+
+    # Jazz
+    SWING = "Swing"
+    JAZZ = "Jazz"
+
+    # Country
+    COUNTRY = "Country"
+    COUNTRY_ROCK = "Country Rock"
+    COUNTRY_POP = "Country Pop"
+
+class FilterType(Enum):
+    NONE = "NONE"
+    TYPE = "TYPE"
+    GENDER = "GENDER"
+
+class FilterValue(Enum):
+    TYPE_GROUP = "Group"
+    TYPE_PERSON = "Person"
+    GENDER_FEMALE = "Female"
+    GENDER_MALE = "Male"
 
 def main():
 
@@ -19,11 +98,18 @@ def main():
     # functions.fetch_artists_by_genre()
 
     #functions.fetch_info_artist(url)
+    #functions.fetch_genre_details_ALL()
+    #functions.fetch_details_others()
+    #functions.fetch_artists_genre()
+    #functions.fetch_genre_details_ALL()
 
-    # functions.fetch_details()
-    functions.fetch_details_others()
+    #functions.create_genre_all_files()
 
 
+
+
+    artists=functions.filter_artists("Rock","all","GENDER","Female",1)
+    print(artists)
 
 
 
